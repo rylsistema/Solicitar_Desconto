a3f976d15946b10268ed8cc082cd9acda633acfd
comercial <rylsistema@gmail.com>
2026-06-26 17:11:16 -0300

    Adiciona busca automática de RCA e atualização das bases JSON

--- converter.ps1
+++ converter.ps1
@@ -21,4 +21,15 @@ $clientesJson = $clientes | ForEach-Object {
 
 $clientesJson | ConvertTo-Json -Depth 10 | Out-File ".\clientes.json" -Encoding utf8
 
+$rcas = Import-Csv ".\Rcas.csv" -Delimiter ";"
+
+$rcasJson = $rcas | ForEach-Object {
+    @{
+        codigo = $_.CODRCA
+        nome   = $_.NOME
+    }
+}
+
+$rcasJson | ConvertTo-Json -Depth 10 | Out-File ".\rcas.json" -Encoding utf8
+
 Write-Host "JSON gerado com sucesso!"
